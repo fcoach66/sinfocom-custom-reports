ce59cc264cd3a7ac5195ea10cf9e085413926e9f
fcoach66 <fabrizio.cocciari@hotmail.it>
2018-11-03 15:45:53 +0100

    Add

---
+++ sinfocom_common_reports/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'Sinfocom Common Report QWeb Views',
+    'version': '1.0',
+    'category': 'Localization/Italy',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Common Report QWeb Views for Sinfocom
+    """,
+    'author':  'fcoach66',
+    'website': 'www.cocciari.com',
+    'images': [
+    ],
+    'depends': [
+        'base',
+        'web',
+	],
+    'data': [
+        'views/report_templates.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
